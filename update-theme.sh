ebbd17767f501af6f300237c6594dc6faa145e14
lwijshoff <195665792+lwijshoff@users.noreply.github.com>
2025-02-04 21:43:29 +0100

    fix: (untested) why display update to newest version even if not updating at all?

--- update-theme.sh
+++ update-theme.sh
@@ -49,20 +49,22 @@ echo "Latest release: $LATEST_RELEASE"
 # Check if the local version is the same as the latest release
 if [ "$CURRENT_VERSION" != "$LATEST_RELEASE" ]; then
     echo "A new release is available. Updating to version $LATEST_RELEASE..."
+    
     git checkout "$LATEST_RELEASE"
     git pull origin "$LATEST_RELEASE"
+
+    # Set the proper ownership and permissions
+    echo "Setting ownership and permissions for the theme..."
+    sudo chown -R www-data:www-data "$THEME_DIR"
+    sudo chmod -R 775 "$THEME_DIR"
+
+    # Print success message
+    clear
+    cat ./assets/ascii-waldjugend-art.txt
+    NEW_VERSION=$(git describe --tags --abbrev=0)
+    printf "\nUpdated to version: %s\n" "$NEW_VERSION"
+    printf "\nTheme update complete!\n"
+
 else
     echo "You are already on the latest release ($LATEST_RELEASE). No update needed."
 fi
-
-# Set the proper ownership and permissions
-echo "Setting ownership and permissions for the theme..."
-sudo chown -R www-data:www-data "$THEME_DIR"
-sudo chmod -R 775 "$THEME_DIR"
-
-# Print a success message
-clear
-cat ./assets/ascii-waldjugend-art.txt
-NEW_VERSION=$(git describe --tags --abbrev=0)
-printf "\nUpdated to version: %s\n" "$NEW_VERSION"
-printf "\nTheme update complete!\n"
