ad93d3698011fa0e3850af338c2164df04964076
lwijshoff <195665792+lwijshoff@users.noreply.github.com>
2025-08-22 21:31:44 +0200

    refactor: nobody cares about pma db

--- src/init-pma.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `pma_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Grant access to root
-GRANT ALL PRIVILEGES ON `pma_db`.* TO 'root'@'%';
-FLUSH PRIVILEGES;
-
--- Create the necessary tables for phpMyAdmin
-USE `pma_db`;
-SOURCE /usr/share/phpmyadmin/sql/create_tables.sql;
